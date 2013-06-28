72547bac32fea55e58841d28d75b0216b16e3e2f
Michael Garate <mpgarate@gmail.com>
2013-06-28 09:24:17 -0400

    cleared rails 3 install to install rails 4

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-StartIt::Application.config.secret_token = 'bd9a84132955371ec6ed57ba56843903cd9ecf877208f97c8c6b166800cab037176cfeed48a19044e7a2cbc34057b6a2a9aaa4f7537ff7768b10a83eb66c642a'
