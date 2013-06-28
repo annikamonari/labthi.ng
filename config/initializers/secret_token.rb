196fc250d69b41d21b14f673f77e297ddfc18a90
Michael Garate <mpgarate@gmail.com>
2013-06-28 10:50:30 -0400

    replaced rails 3 with 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StartIt::Application.config.secret_key_base = '009579420e27dc186900fc5b91aa7e2ec532d497c75bb84d8b612ce29fab40b0d56be6ce1c7d8b1496cf8060e134b6ca1868fe5ad3087ec7d03a6fcbb3dc8619'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StartIt::Application.config.session_store :cookie_store, key: '_start_it_session'
