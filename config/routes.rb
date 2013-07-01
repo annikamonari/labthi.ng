8df6b5268b274e92fc946e6ddca9e9f9defb19eb
Michael Garate <mpgarate@gmail.com>
2013-06-30 21:44:59 -0400

    added devise authentication and started user_specs for auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 StartIt::Application.routes.draw do
+  get "home/index"
+  devise_for :users
   resources :ideas
+
+  root 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
