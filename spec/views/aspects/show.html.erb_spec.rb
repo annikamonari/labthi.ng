b35b603919cb112003553182fdd54248bf9f6bc1
Michael Garate <mpgarate@gmail.com>
2013-07-06 10:10:47 -0400

    modified Aspect#show test to associate with idea

--- spec/views/aspects/show.html.erb_spec.rb
+++ spec/views/aspects/show.html.erb_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
+
+def create_idea
+  valid_attributes = { "phase" => "1", "brief" => "lorem ipsum dolor sit amet", "industry" => "Technology"}
+  idea = Idea.create! valid_attributes
+end
+
 describe "aspects/show" do
   before(:each) do
+    idea = create_idea
     @aspect = assign(:aspect, stub_model(Aspect,
-      :brief => "MyText"
+      :title => "MyTitle",
+      :brief => "MyText",
+      :idea => idea
     ))
   end
 
@@ -11,5 +20,6 @@ describe "aspects/show" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/MyText/)
+    rendered.should match(/MyTitle/)
   end
 end
