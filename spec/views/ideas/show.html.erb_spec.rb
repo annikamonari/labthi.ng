b7eb812dff5d7183e52961d278a4b0965d3c01f8
Michael Garate <mpgarate@gmail.com>
2013-07-07 11:21:20 -0400

    display user email with Aspect on idea page, wrote some spec for viewing aspects on an idea

--- spec/views/ideas/show.html.erb_spec.rb
+++ spec/views/ideas/show.html.erb_spec.rb
@@ -1,20 +1,15 @@
 require 'spec_helper'
 
-def create_aspect
-  user = FactoryGirl.create(:user)
-  valid_attributes = { "phase" => "1", "brief" => "lorem ipsum dolor sit amet", "industry" => "Technology", "user_id" => user}
-  idea = Aspect.create! valid_attributes
-end
-
 describe "ideas/show" do
   before(:each) do
-    @idea = assign(:idea, stub_model(Idea,
+    @idea = FactoryGirl.create(
+      :idea,
       :phase => 1,
       :brief => "MyText",
       :image => "Image",
       :active => false,
       :industry => "Industry"
-    ))
+    )
   end
 
   it "renders attributes in <p>" do
@@ -25,5 +20,17 @@ describe "ideas/show" do
     rendered.should match(/Image/)
     rendered.should match(/false/)
     rendered.should match(/Industry/)
+  end
+
+  it "displays any aspects" do
+    @aspect = FactoryGirl.create(
+        :aspect,
+        :title => 'Valid title',
+        :brief => 'Valid brief',
+        :idea_id => @idea
+    )
+    render
+    rendered.should match(/Valid title/)
+    rendered.should match(/Valid brief/)
   end
 end
