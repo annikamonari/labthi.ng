1050a552ee084e4fa8beb5e3d191ff5f3e88a6bc
vagrant <mpgarate@gmail.com>
2013-07-14 06:23:12 +0000

    configured solution and aspect factories

--- spec/factories/aspect.rb
+++ spec/factories/aspect.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :aspect do
     title 'Valid title'
     brief 'Valid brief'
-    idea_id { |c| c.association(:idea) }
-    user_id { |c| c.association(:user) }
+    idea { |c| c.association(:idea) }
+    user { |c| c.association(:user) }
   end
 end
