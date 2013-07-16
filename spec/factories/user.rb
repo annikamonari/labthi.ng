8956da5a60d32635cf04662429f418339248d78a
vagrant <mpgarate@gmail.com>
2013-07-16 02:54:16 +0000

    display solution on its aspect view

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) { |n| "person#{n}@example.com" }
+    email { "person#{Time.new.usec}@example.com" }
     password               '12345678'
     password_confirmation  '12345678'
   end
