9dc93c04eed217b42940cda868fd1cbfaf02ca5f
vagrant <mpgarate@gmail.com>
2013-07-26 01:07:03 +0000

    updated spec feature session helpers to not check for name at login

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -14,7 +14,6 @@ module Features
       user = create(:user)
       visit '/login'
       fill_in 'Email', with: user.email
-      fill_in 'Name', with: user.name
       fill_in 'Password', with: user.password
       click_button 'Sign in'
     end
