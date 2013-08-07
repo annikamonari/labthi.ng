57a7a6a60307f14aa49113d09b77c119060bb780
vagrant <mpgarate@gmail.com>
2013-08-07 00:09:28 +0000

    cleaned up routing and fixed sign_in path bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,30 +2,28 @@ StartIt::Application.routes.draw do
   
   get "activities/index"
   resources :comments
-
   resources :solutions
-
   resources :idea_tags
-  resources :users, :except => [:create, :destroy]
   resources :aspects
   resources :ideas
 
+  # Users and Admins
+  devise_for :users do
+    get "users/sign_in", :to => "devise/sessions#new"
+  end
+  resources :users, :only => [:show]
   devise_for :admins
-  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-  
-  get 'show/:id' => 'user#show'
-  get "home/index"
 
-  devise_for :users
 
   devise_scope :user do
     get "register", :to => "devise/registrations#new"
     get "login", :to => "devise/sessions#new"
-    #get "profile", :to => "devise/registrations#edit"
   end
 
-
-
+  #Rails Admin interface
+  mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
+  
+  get "home/index"
   root to: 'home#index'
   
 end
