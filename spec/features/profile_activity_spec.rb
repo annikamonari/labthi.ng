a0cb2cd4a29edd927a1cf28f35c73bd9a87b0149
vagrant <mpgarate@gmail.com>
2013-08-15 01:59:28 +0000

    added profile update to activity stream

---
+++ spec/features/profile_activity_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+feature 'Visitor checks activity stream' do
+  scenario 'for a recently updated profile' do
+  	@user = sign_in
+  	click_link 'Profile'
+  	click_button 'Update Profile'
+    visit '/activities/index'
+    expect(page).to have_content(@user.name + " updated profile information")
+  end
+end
