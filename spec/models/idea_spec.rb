e6954cc1259539a67d72b328075f3b5227711531
vagrant <mpgarate@gmail.com>
2013-08-19 01:43:51 +0000

    added failing spec for idea>aspect dependent destroy

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -2,20 +2,27 @@ require 'spec_helper'
 
 describe Idea do
   it "is not valid without a phase" do
-    article = Idea.create(phase: nil)
-    article.should_not be_valid
+    idea = Idea.create(phase: nil)
+    idea.should_not be_valid
   end
   it "is not valid without a brief" do
-    article = Idea.create(brief: nil)
-    article.should_not be_valid
+    idea = Idea.create(brief: nil)
+    idea.should_not be_valid
   end
   it "is not valid without an industry" do
-    article = Idea.create(industry: nil)
-    article.should_not be_valid
+    idea = Idea.create(industry: nil)
+    idea.should_not be_valid
   end
   it "is valid with a phase, brief, and industry" do
-    article = Idea.create(phase:1, brief: "A website for crowdsourcing startup ideas", industry: "Internet")
-    article.should be_valid
+    idea = Idea.create(phase:1, brief: "A website for crowdsourcing startup ideas", industry: "Internet")
+    idea.should be_valid
+  end
+
+  it "should destroy associated profile" do
+    idea = Idea.create(phase:1, brief: "A website for crowdsourcing startup ideas", industry: "Internet")
+      expect {
+        idea.destroy
+      }.to change(Aspect, :count).by(-1)
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -63,13 +63,6 @@ describe User do
     end
 
     it "should destroy associated profile" do
-=begin
-      profile = @user.profile
-      expect(Profile.find(profile.id)).to_not be_nil
-      @user.destroy
-      expect(User.find(@user.id)).to be_nil
-      expect(Profile.find(profile.id)).to be_nil
-=end
       expect {
         @user.destroy
       }.to change(Profile, :count).by(-1)
