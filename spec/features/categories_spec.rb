3615630bc499e4437b9da89b4de1dba8af141dc5
vagrant <mpgarate@gmail.com>
2013-08-29 20:32:35 +0000

    renamed spec for idea categories

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-feature 'User updates an idea' do
-  scenario 'with a category' do
+feature 'Categories' do
+  scenario 'User tags an idea with a category' do
     submit_idea
     select 'Science & Technology', :from => 'Categories'
     update_idea
