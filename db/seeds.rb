08ecf55c491c3d198eaa68f449ea0c490139b295
vagrant <mpgarate@gmail.com>
2013-09-15 11:27:46 -0400

    inserted some default aspects and allow for updating seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,39 +6,75 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-5.times do |i|
-  u = User.new(
-      :email => "email#{Time.new.usec}@domain.com",
-      :password => '12341234',
-      :name => "Robot R2D#{i}"
-  )
-  u.save!
-  FactoryGirl.create(:comment)
+if User.count == 0
+	5.times do |i|
+	  u = User.new(
+	      :email => "email#{Time.new.usec}@domain.com",
+	      :password => '12341234',
+	      :name => "Robot R2D#{i}"
+	  )
+	  u.save!
+	  FactoryGirl.create(:comment)
+	end
+end
+
+aspect_data = [
+	{
+		brief: "Problems & Solutions"
+	},
+	{
+		brief: "Key Activities"		
+	},
+	{
+		brief: "Key Resources"		
+	},
+	{
+		brief: "Vaue Propositions"		
+	},
+	{
+		brief: "Customer Relationship"		
+	},
+	{
+		brief: "Channels"		
+	},
+	{
+		brief: "Customer Segments"		
+	},
+	{
+		brief: "Cost Structure"		
+	},
+	{
+		brief: "Revenue Streams"		
+	}
+]
+
+aspect_data.each do |d|
+	Aspect.find_or_create_by(d)
 end
 
 
 
-FactoryGirl.create(:aspect, brief: "Problems & Solutions")
+admin_data = [
+  { 
+		name: "Michael G",
+		email: "mpgarate@gmail.com",
+		password: "banana123",
+		password_confirmation: "banana123"
+	},
+	{
+		name: "Annika M",
+		email: "annikamonari@gmail.com",
+		password: "banana123",
+		password_confirmation: "banana123"
+	},
+	{
+		name: "Emily P",
+		email: "emily.pakulski@gmail.com",
+		password: "banana123",
+		password_confirmation: "banana123"
+	}
+]
 
-
-
-FactoryGirl.create(:admin,
-	name: "Michael G",
-	email: "mpgarate@gmail.com",
-	password: "banana123",
-	password_confirmation: "banana123"
-	)
-
-FactoryGirl.create(:admin, 
-	name: "Annika M",
-	email: "annikamonari@gmail.com",
-	password: "banana123",
-	password_confirmation: "banana123"
-	)
-
-FactoryGirl.create(:admin, 
-	name: "Emily P",
-	email: "emily.pakulski@gmail.com",
-	password: "banana123",
-	password_confirmation: "banana123"
-	)
+admin_data.each do |d|
+	FactoryGirl.create(:admin, d) unless Admin.where(d)
+end
