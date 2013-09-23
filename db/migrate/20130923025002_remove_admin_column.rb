9e4057fc8bbde3008c55715ea1363e1655363139
vagrant <mpgarate@gmail.com>
2013-09-22 22:53:50 -0400

    removed admin table from database

---
+++ db/migrate/20130923025002_remove_admin_column.rb
@@ -0,0 +1,5 @@
+class RemoveAdminColumn < ActiveRecord::Migration
+ 	def change
+    drop_table :admins
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130923013802) do
+ActiveRecord::Schema.define(version: 20130923025002) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -58,22 +58,6 @@ ActiveRecord::Schema.define(version: 20130923013802) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "admins", force: true do |t|
-    t.string   "email",              default: "", null: false
-    t.string   "encrypted_password", default: "", null: false
-    t.integer  "sign_in_count",      default: 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.integer  "failed_attempts",    default: 0
-    t.string   "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name"
-  end
 
   create_table "answers", force: true do |t|
     t.text     "brief"
