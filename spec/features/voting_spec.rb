0fa9f92304cc6dc066031fc63a7fc30f593e003e
vagrant <mpgarate@gmail.com>
2013-09-29 20:09:43 -0400

    vote down an idea

--- spec/features/voting_spec.rb
+++ spec/features/voting_spec.rb
@@ -12,5 +12,11 @@ feature 'Visitor votes' do
   	click_link 'vote-up'
   	page.should have_content("Votes: 1")
   end
+  scenario 'against an idea' do
+  	visit url_for(@idea)
+  	page.should have_content("Votes: 0")
+  	click_link 'vote-down'
+  	page.should have_content("Votes: -1")
+  end
 
 end
