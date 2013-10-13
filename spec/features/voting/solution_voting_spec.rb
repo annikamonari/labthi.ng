b66cc45bdfd2c0a11da72fcb6f7b61a5e1d4b749
vagrant <mpgarate@gmail.com>
2013-10-12 23:38:20 -0400

    aspect solutions are sorted by votes

--- spec/features/voting/solution_voting_spec.rb
+++ spec/features/voting/solution_voting_spec.rb
@@ -2,21 +2,27 @@ require 'spec_helper'
 
 feature 'Visitor votes' do
   before(:each) do
-  	@solution = FactoryGirl.create(:solution)
+    @aspect = FactoryGirl.create(:aspect)
+    @idea = FactoryGirl.create(:idea)
+    @solution_before = FactoryGirl.create(:solution, idea: @idea, aspect: @aspect)
+    @solution = FactoryGirl.create(:solution, idea: @idea, aspect: @aspect)
+    @solution_after = FactoryGirl.create(:solution, idea: @idea, aspect: @aspect)
     @selector = ".solution-#{@solution.id}-vote-wrapper"
   	sign_in
-    visit idea_aspect_path(idea_id:@solution.idea.id, id: @solution.aspect.id)
+    visit idea_aspect_path(idea_id:@solution.idea.id, id: @aspect.id)
   end
 
   scenario 'in favor of a solution' do
   	find(@selector).should have_content("Votes: 0")
   	click_link "vote-up-solution-#{@solution.id}"
   	find(@selector).should have_content("Votes: 1")
+    page.should have_selector("ul.solutions li:first-child #{@selector}", :text => "Votes: 1")
   end
   scenario 'against a solution' do
   	find(@selector).should have_content("Votes: 0")
   	click_link "vote-down-solution-#{@solution.id}"
   	find(@selector).should have_content("Votes: -1")
+    page.should have_selector("ul.solutions li:last-child #{@selector}", :text => "Votes: -1")
   end
   scenario 'to undo an upvote on a solution' do
     click_link "vote-up-solution-#{@solution.id}"
