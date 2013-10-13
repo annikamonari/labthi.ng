f2e89a5d7a7663de00db6400902b3ebfee852172
vagrant <mpgarate@gmail.com>
2013-10-12 23:54:33 -0400

    sort questions by votes

--- spec/features/voting/question_voting_spec.rb
+++ spec/features/voting/question_voting_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 feature 'Visitor votes' do
   before(:each) do
-  	@question = FactoryGirl.create(:question)
+    @question_before = FactoryGirl.create(:question)
+    @question = FactoryGirl.create(:question)
+    @question_after = FactoryGirl.create(:question)
     @selector = ".question-#{@question.id}-vote-wrapper"
   	sign_in
     visit url_for(@question.idea)
@@ -12,11 +14,13 @@ feature 'Visitor votes' do
   	find(@selector).should have_content("Votes: 0")
   	click_link "vote-up-question-#{@question.id}"
   	find(@selector).should have_content("Votes: 1")
+    page.should have_selector("ul.questions li:first-child #{@selector}", :text => "Votes: 1")
   end
   scenario 'against a question' do
   	find(@selector).should have_content("Votes: 0")
   	click_link "vote-down-question-#{@question.id}"
   	find(@selector).should have_content("Votes: -1")
+    page.should have_selector("ul.questions li:last-child #{@selector}", :text => "Votes: -1")
   end
   scenario 'to undo an upvote on a question' do
     click_link "vote-up-question-#{@question.id}"
