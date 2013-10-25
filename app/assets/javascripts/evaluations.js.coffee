735df0573fad11a62a0551361c50a29420f419e3
vagrant <mpgarate@gmail.com>
2013-10-25 08:55:48 -0400

    some js work for voting

--- app/assets/javascripts/evaluations.js.coffee
+++ app/assets/javascripts/evaluations.js.coffee
@@ -1,3 +1,13 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+
+$(document).on "page:change", ->
+		console.log("got to this one")
+
+	$("a.vote").click ->
+		$(this).toggleClass("active")
+		console.log("yo")
+		alert("clicked")
--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131022014301) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
     t.text     "body"
@@ -231,6 +228,11 @@ ActiveRecord::Schema.define(version: 20131022014301) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  add_foreign_key "answers", "questions", name: "solutions_question_id_fk"
+  add_foreign_key "answers", "users", name: "solutions_user_id_fk"
+
+  add_foreign_key "comments", "users", name: "comments_user_id_fk"
 
   add_foreign_key "ideas", "users", name: "ideas_user_id_fk"
 
