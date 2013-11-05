365e7e9fa5139bcbdb50156bd5030c4057f6b83e
vagrant <mpgarate@gmail.com>
2013-11-05 00:16:08 -0500

    get points from upvoted question, answer, solution, comment

--- spec/features/reputation/rep_from_upvote_spec.rb
+++ spec/features/reputation/rep_from_upvote_spec.rb
@@ -19,27 +19,25 @@ feature 'Visitor gets reputation from' do
     visit profile_path @question.user
     find(".rep-points").should have_content '5'
   end
-=begin
   scenario 'an answer upvoted' do
     @answer = FactoryGirl.create(:answer)
     visit question_path @answer.question
-    click_link "vote-down-answer-#{@answer.id}"
-    find('.show-user-profile a').click
-    find(".rep-points").should have_content '-5'
+    click_link "vote-up-answer-#{@answer.id}"
+    visit profile_path @answer.user
+    find(".rep-points").should have_content '10'
   end
   scenario 'a solution upvoted' do
     @solution = FactoryGirl.create(:solution)
     visit idea_aspect_path @solution.idea, @solution.aspect
-    click_link "vote-down-solution-#{@solution.id}"
-    find('.show-user-profile a').click
-    find(".rep-points").should have_content '-5'
+    click_link "vote-up-solution-#{@solution.id}"
+    visit profile_path @solution.user
+    find(".rep-points").should have_content '10'
   end
   scenario 'a comment upvoted' do
     @comment = FactoryGirl.create(:comment)
     visit question_path @comment.commentable.question
-    click_link "vote-down-comment-#{@comment.id}"
-    find('.show-user-profile a').click
-    find(".rep-points").should have_content '-1'
+    click_link "vote-up-comment-#{@comment.id}"
+    visit profile_path @comment.user
+    find(".rep-points").should have_content '1'
   end
-=end
 end
