585c43ce1d1697d751c2adab70178683c49ac750
vagrant <mpgarate@gmail.com>
2013-11-09 18:45:14 -0500

    preliminary styling on navbar

--- spec/features/reputation/rep_from_downvote_spec.rb
+++ spec/features/reputation/rep_from_downvote_spec.rb
@@ -10,34 +10,34 @@ feature 'Visitor gets reputation from' do
     visit idea_path @idea
     click_link "vote-down-idea-#{@idea.id}"
     find('.show-user-profile a').click
-    find(".rep-points").should have_content '-10'
+    find(".sidebar .rep-points").should have_content '-10'
   end
   scenario 'downvoting a question' do
     @question = FactoryGirl.create(:question)
     visit idea_path @question.idea
     click_link "vote-down-question-#{@question.id}"
     find('.show-user-profile a').click
-    find(".rep-points").should have_content '-3'
+    find(".sidebar .rep-points").should have_content '-3'
   end
   scenario 'downvoting an answer' do
     @answer = FactoryGirl.create(:answer)
     visit question_path @answer.question
     click_link "vote-down-answer-#{@answer.id}"
     find('.show-user-profile a').click
-    find(".rep-points").should have_content '-5'
+    find(".sidebar .rep-points").should have_content '-5'
   end
   scenario 'downvoting a solution' do
     @solution = FactoryGirl.create(:solution)
     visit idea_aspect_path @solution.idea, @solution.aspect
     click_link "vote-down-solution-#{@solution.id}"
     find('.show-user-profile a').click
-    find(".rep-points").should have_content '-5'
+    find(".sidebar .rep-points").should have_content '-5'
   end
   scenario 'downvoting a comment' do
     @comment = FactoryGirl.create(:comment)
     visit question_path @comment.commentable.question
     click_link "vote-down-comment-#{@comment.id}"
     find('.show-user-profile a').click
-    find(".rep-points").should have_content '-1'
+    find(".sidebar .rep-points").should have_content '-1'
   end
 end
--- spec/features/reputation/rep_from_upvote_spec.rb
+++ spec/features/reputation/rep_from_upvote_spec.rb
@@ -10,34 +10,34 @@ feature 'Visitor gets reputation from' do
     visit idea_path @idea
     click_link "vote-up-idea-#{@idea.id}"
     visit profile_path @idea.user
-    find(".rep-points").should have_content '10'
+    find(".sidebar .rep-points").should have_content '10'
   end
   scenario 'a question upvoted' do
     @question = FactoryGirl.create(:question)
     visit idea_path @question.idea
     click_link "vote-up-question-#{@question.id}"
     visit profile_path @question.user
-    find(".rep-points").should have_content '5'
+    find(".sidebar .rep-points").should have_content '5'
   end
   scenario 'an answer upvoted' do
     @answer = FactoryGirl.create(:answer)
     visit question_path @answer.question
     click_link "vote-up-answer-#{@answer.id}"
     visit profile_path @answer.user
-    find(".rep-points").should have_content '10'
+    find(".sidebar .rep-points").should have_content '10'
   end
   scenario 'a solution upvoted' do
     @solution = FactoryGirl.create(:solution)
     visit idea_aspect_path @solution.idea, @solution.aspect
     click_link "vote-up-solution-#{@solution.id}"
     visit profile_path @solution.user
-    find(".rep-points").should have_content '10'
+    find(".sidebar .rep-points").should have_content '10'
   end
   scenario 'a comment upvoted' do
     @comment = FactoryGirl.create(:comment)
     visit question_path @comment.commentable.question
     click_link "vote-up-comment-#{@comment.id}"
     visit profile_path @comment.user
-    find(".rep-points").should have_content '1'
+    find(".sidebar .rep-points").should have_content '1'
   end
 end
