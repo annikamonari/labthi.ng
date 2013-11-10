b12f1bd41927a0650ae9907df55c8d827c2981a7
vagrant <mpgarate@gmail.com>
2013-11-09 22:49:36 -0500

    fix rotation quirk with carrierwave

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -58,4 +58,13 @@ class PhotoUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
+  process :rotate
+  def rotate
+    manipulate! do |img|
+      img.auto_orient
+      img = yield(img) if block_given?
+      img
+    end
+  end
+
 end
