41f5aa67e6625f02b007d15a6b41c5674d35301d
vagrant <mpgarate@gmail.com>
2013-11-30 21:58:32 -0500

    added rep switch/undo specs

---
+++ spec/features/reputation/rep_after_switching_vote_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+feature 'Visitor gets reputation from' do
+  before(:each) do
+  	sign_in
+  end
+
+  scenario 'switching an idea vote' do
+  	@idea = FactoryGirl.create(:idea)
+    visit idea_path @idea
+    click_link "vote-down-idea-#{@idea.id}"
+    click_link "vote-up-idea-#{@idea.id}"
+    find('.show-user-profile a').click
+    find(".sidebar .rep-points").should have_content '10'
+  end
+  scenario 'switching a question vote' do
+    @question = FactoryGirl.create(:question)
+    visit idea_path @question.idea
+    click_link "vote-down-question-#{@question.id}"
+    click_link "vote-up-question-#{@question.id}"
+    find('.show-user-profile a').click
+    find(".sidebar .rep-points").should have_content '5'
+  end
+  scenario 'switching an answer vote' do
+    @answer = FactoryGirl.create(:answer)
+    visit question_path @answer.question
+    click_link "vote-down-answer-#{@answer.id}"
+    click_link "vote-up-answer-#{@answer.id}"
+    find('.show-user-profile a').click
+    find(".sidebar .rep-points").should have_content '10'
+  end
+  scenario 'switching a solution vote' do
+    @solution = FactoryGirl.create(:solution)
+    visit idea_aspect_path @solution.idea, @solution.aspect
+    click_link "vote-down-solution-#{@solution.id}"
+    click_link "vote-up-solution-#{@solution.id}"
+    find('.show-user-profile a').click
+    find(".sidebar .rep-points").should have_content '10'
+  end
+  scenario 'switching a comment vote' do
+    @comment = FactoryGirl.create(:comment)
+    visit question_path @comment.commentable.question
+    click_link "vote-down-comment-#{@comment.id}"
+    click_link "vote-up-comment-#{@comment.id}"
+    find('.show-user-profile a').click
+    find(".sidebar .rep-points").should have_content '1'
+  end
+end
---
+++ spec/features/reputation/rep_after_undoing_vote_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+feature 'Visitor gets reputation from' do
+  before(:each) do
+  	sign_in
+  end
+
+  scenario 'undoing an idea vote' do
+  	@idea = FactoryGirl.create(:idea)
+    visit idea_path @idea
+    click_link "vote-down-idea-#{@idea.id}"
+    click_link "vote-undo-idea-#{@idea.id}"
+    find('.show-user-profile a').click
+    find(".sidebar .rep-points").should have_content '0'
+  end
+  scenario 'undoing a question vote' do
+    @question = FactoryGirl.create(:question)
+    visit idea_path @question.idea
+    click_link "vote-down-question-#{@question.id}"
+    click_link "vote-undo-question-#{@question.id}"
+    find('.show-user-profile a').click
+    find(".sidebar .rep-points").should have_content '0'
+  end
+  scenario 'undoing an answer vote' do
+    @answer = FactoryGirl.create(:answer)
+    visit question_path @answer.question
+    click_link "vote-down-answer-#{@answer.id}"
+    click_link "vote-undo-answer-#{@answer.id}"
+    find('.show-user-profile a').click
+    find(".sidebar .rep-points").should have_content '0'
+  end
+  scenario 'undoing a solution vote' do
+    @solution = FactoryGirl.create(:solution)
+    visit idea_aspect_path @solution.idea, @solution.aspect
+    click_link "vote-down-solution-#{@solution.id}"
+    click_link "vote-undo-solution-#{@solution.id}"
+    find('.show-user-profile a').click
+    find(".sidebar .rep-points").should have_content '0'
+  end
+  scenario 'undoing a comment vote' do
+    @comment = FactoryGirl.create(:comment)
+    visit question_path @comment.commentable.question
+    click_link "vote-down-comment-#{@comment.id}"
+    click_link "vote-undo-comment-#{@comment.id}"
+    find('.show-user-profile a').click
+    find(".sidebar .rep-points").should have_content '0'
+  end
+end
