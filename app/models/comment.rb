ecafe9e3ef447854d12fc1351f530a92779cdbaa
vagrant <mpgarate@gmail.com>
2013-12-02 00:59:20 -0500

    removed old reputation from comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,7 +11,5 @@ class Comment < ActiveRecord::Base
 	belongs_to :comment
 	belongs_to :commentable, :polymorphic => true
 	has_many :comments, as: :commentable, :dependent => :destroy
-  	
-  has_reputation :votes, source: :user, aggregated_by: :sum
 
 end
