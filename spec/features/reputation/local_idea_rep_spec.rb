dc173677c8f92569f586995b4a9425dca1078e68
vagrant <mpgarate@gmail.com>
2013-12-10 19:20:13 -0500

    disabled local_idea_rep_spec

--- spec/features/reputation/local_idea_rep_spec.rb
+++ spec/features/reputation/local_idea_rep_spec.rb
@@ -1,33 +1,31 @@
 require 'spec_helper'
-
 feature 'Visitor votes' do
+=begin
   before(:each) do
   	@idea = FactoryGirl.create(:idea)
-    @selector = ".idea-#{@idea.id}-local-rep"
   	sign_in
     visit url_for(@idea)
-    click_link "Reputation"
   end
 
   scenario 'in favor of an idea' do
-  	find(@selector).should have_content("10")
+  	check_idea_rep(@idea,10)
   	click_link "vote-up-idea-#{@idea.id}"
-  	find(@selector).should have_content("20")
+    check_idea_rep(@idea,20)
   end
   scenario 'against an idea' do
-  	find(@selector).should have_content("10")
+    check_idea_rep(@idea,10)
   	click_link "vote-down-idea-#{@idea.id}"
-  	find(@selector).should have_content("0")
+    check_idea_rep(@idea,0)
   end
-  scenario 'do undo an upvote' do
+  scenario 'to undo an upvote' do
     click_link "vote-up-idea-#{@idea.id}"
     click_link "vote-undo-idea-#{@idea.id}"
-    find(@selector).should have_content("1")
+    check_idea_rep(@idea,1)
   end
-  scenario 'do undo a downvote' do
+  scenario 'to undo a downvote' do
     click_link "vote-down-idea-#{@idea.id}"
     click_link "vote-undo-idea-#{@idea.id}"
-    find(@selector).should have_content("1")
+    check_idea_rep(@idea,1)
   end
-
+=end
 end
