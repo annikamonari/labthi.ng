fc5b5c07f40cdcae7f45a2c1336784ac102f8cee
vagrant <mpgarate@gmail.com>
2013-12-23 16:09:40 -0500

    made idea category links clickable

--- app/helpers/ideas_helper.rb
+++ app/helpers/ideas_helper.rb
@@ -1,2 +1,13 @@
 module IdeasHelper
+	def print_categories
+    suppress_comma = true
+    print = ""
+    @idea.category_list.each do |category|
+      print += ", " unless suppress_comma
+      suppress_comma = false
+      print += link_to category, { controller: 'create', category: category }, class: 'category-tag'
+    end
+
+    return print.html_safe
+	end
 end
