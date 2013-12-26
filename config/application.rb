f359262f234559a954ebba397b8e6ca2d0270513
vagrant <mpgarate@gmail.com>
2013-12-26 14:05:43 -0500

    reversed previous commit

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,6 @@ module StartIt
     # config.i18n.default_locale = :de
 
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.assets.initialize_on_precompile = false
 
     config.generators do |g|
       g.test_framework :rspec,
