664cef1aef6b4e6611e29a1d6ff55f2a4d2e9755
vagrant <mpgarate@gmail.com>
2014-02-20 21:40:32 -0500

    follow button works

--- spec/features/following_spec.rb
+++ spec/features/following_spec.rb
@@ -1,19 +1,19 @@
 require 'spec_helper'
 
-feature 'User can' do
+feature 'User can', js: true do
   scenario 'follow an idea' do
     sign_in
     @idea = FactoryGirl.create(:idea)
-    visit url_for(@idea)
-    click_button("Follow")
-    page.should have_content "Unfollow"
+    visit "/ideas/#{@idea.id}"
+    page.find('.follow-idea-button').click
+    page.should have_css ".unfollow-idea-button"
   end
   scenario 'unfollow an idea' do
     sign_in
     @idea = FactoryGirl.create(:idea)
-    visit url_for(@idea)
-    click_button("Follow")
-    click_button("Unfollow")
-    page.should have_content "Follow"
+    visit "/ideas/#{@idea.id}"
+    click_button "Follow"
+    page.find('.unfollow-idea-button').click
+    page.should have_css ".follow-idea-button"
   end
 end
