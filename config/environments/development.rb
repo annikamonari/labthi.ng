5e5ce13ff515d48aa2e3c819a62b4a635d144e5f
Annika Monari <annikamonari@gmail.com>
2014-04-16 15:04:44 +0100

    fixed reset passwords for production mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,11 +43,12 @@ StartIt::Application.configure do
         :user_name => 'labthi.ng.adm1n.passw0rd.reset@gmail.com',
         :password => 'annikamike123'
   }
+  
   config.after_initialize do
-  Bullet.enable = true
-  Bullet.alert = true
-  Bullet.bullet_logger = true
-  Bullet.console = true
-  Bullet.rails_logger = true
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ StartIt::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => 'labthi.ng.heroku.com' }
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
