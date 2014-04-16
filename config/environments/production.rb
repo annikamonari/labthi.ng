28646053a7633929bb091357da3c35929ae012ed
Annika Monari <annikamonari@gmail.com>
2014-04-16 18:56:49 +0100

    final attempt to fix staging email sending

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ StartIt::Application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.default_url_options = { :host => 'labthing-staging.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'labthing-staging.herokuapp.com'
+  
   
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
