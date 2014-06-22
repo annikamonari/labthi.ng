d1c49b454c22124477adb371f05a2c2c76ccb656
vagrant <mpgarate@gmail.com>
2014-06-22 16:27:08 -0400

    parts post update

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -8,11 +8,26 @@ class PartsController < ApplicationController
     @part.value = nil
     @part.user = nil
     @part.save
+
     redirect_to @part.idea_build
+  end
+
+  def update
+    respond_to do |format|
+      if @part.update(part_params)
+        format.html {redirect_to @part.idea_build}
+      else
+        format.html {redirect_to @part.idea_build}
+      end
+    end
   end
 
   def set_part
     @part = Part.find(params[:id])
+  end
+
+  def part_params
+    params.require(:part).permit(:user, :value)
   end
 
 end
--- spec/controllers/parts_controller_spec.rb
+++ spec/controllers/parts_controller_spec.rb
@@ -7,6 +7,7 @@ describe PartsController do
       @part = create(:part)
       @idea = @part.idea
     end
+
     it "returns http success" do
       get :edit, id: @part, idea_id: @part.idea
       response.should render_template :edit
@@ -17,11 +18,31 @@ describe PartsController do
     it "returns http success" do
       part = create(:part, value: "valid value")
       delete :clear, id: part, idea_id: part.idea
-      
+
       part.reload
 
       expect(part.value).to be nil
       expect(part.user).to be nil
+    end
+  end
+
+  describe "PUT 'update'" do
+    before(:each) do
+      @part = create(:part)
+      @idea = @part.idea
+    end
+
+    it "locates the requested part" do
+      put :update, idea_id: @idea, id: @part, part: attributes_for(:part)
+      assigns(:part).should eq(@part)
+    end
+
+    it "changes @part's attributes" do
+      put :update, idea_id: @idea, id: @part, part: {value: "lorem ipsum dolor"}
+
+      @part.reload
+      
+      expect(@part.value).to eq("lorem ipsum dolor")
     end
   end
 
