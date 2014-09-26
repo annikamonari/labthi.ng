6cf18e6612fdbc63c4d25a583f697e7c2d7a175f
Alan <alan.vey12@imperial.ac.uk>
2014-09-26 22:58:06 +0100

    Testing complete and bug fixes

--- spec/features/part_spec.rb
+++ spec/features/part_spec.rb
@@ -10,20 +10,30 @@ feature 'User visits the build page' do
 
   scenario 'and finds a bootsy area to type in' do
     # Check for a class specific to bootsy
-    click_link 'Idea Scope Summary'
+    click_link 'Idea Description'
     page.html =~ /wysihtml5-toolbar/
   end
 
   scenario 'and saves work once finished' do
-    click_link 'Idea Scope Summary'
+    click_link 'Idea Description'
     fill_in 'part_value', :with => 'some text'
     click_button "Save part"
     @part.value == 'some_text'
   end
 
   scenario 'and click start on a part' do
-    click_button 'Start'
-    expect(page).should have_content('Finish')
-    expect(page).should have_content('Started')
+    click_button "1"
+    expect(page).to have_selector(:link_or_button, 'Finish')
+    expect(page).to have_content('Started')
+  end
+
+  scenario 'and button vanishes when status is accepted' do
+    click_button "1"
+    click_button "1"
+    click_button "1"
+    click_button "1"
+
+    expect(page).not_to have_selector(:link_or_button, 'Accept')
+    expect(page).to have_content('Accepted')
   end
 end
