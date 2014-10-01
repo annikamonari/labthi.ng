211d2eed05373ed962991ecae7e110478095caa9
Alan <alan.vey12@imperial.ac.uk>
2014-10-01 15:59:18 +0100

    Implemented Equity

--- app/models/component/business_plan_component.rb
+++ app/models/component/business_plan_component.rb
@@ -2,16 +2,16 @@ class BusinessPlanComponent < Component
   
   def create_required_parts
     names = [
-      "Summary of Business",
-      "Summary of Product",
-      "Marketing Strategy Plan",
-      "Market Analysis",
-      "Marketing Strategy Implementation",
-      "Executive Summary"
+      ["Summary of Business", "6%"],
+      ["Summary of Product", "6%"],
+      ["Marketing Strategy Plan", "8%"],
+      ["Market Analysis", "8%"],
+      ["Marketing Strategy Implementation", "8%"],
+      ["Executive Summary", "6%"]
     ]
 
-    names.each do |name|
-      self.parts.create(name:name, status:'Unstarted')
+    names.each do |name, equity|
+      self.parts.create(name:name, status:'Unstarted', equity:equity)
     end
   end
 end
--- app/models/component/prototype_component.rb
+++ app/models/component/prototype_component.rb
@@ -2,12 +2,12 @@ class PrototypeComponent < Component
   
   def create_required_parts
     names = [
-      "Flowchart and Schema",
-      "Prototype"
+      ["Flowchart and Schema", "6%"],
+      ["Prototype", "20%"]
     ]
 
-    names.each do |name|
-      self.parts.create(name:name, status:'Unstarted')
+    names.each do |name, equity|
+      self.parts.create(name:name, status:'Unstarted', equity:equity)
     end
   end
 end
