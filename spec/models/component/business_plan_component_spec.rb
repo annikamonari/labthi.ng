28c8b8d261c016d0c645e3bd50fa793faef94dcf
Alan <alan.vey12@imperial.ac.uk>
2014-10-01 16:51:26 +0100

    Updates tests for fewer parts

--- spec/models/component/business_plan_component_spec.rb
+++ spec/models/component/business_plan_component_spec.rb
@@ -5,7 +5,7 @@ describe BusinessPlanComponent do
   it "creates its required parts" do
     expect{
       business_plan_component = create(:business_plan_component)
-      }.to change(Part, :count).by(8)
+      }.to change(Part, :count).by(6)
   end
 
   context "method implementations" do
