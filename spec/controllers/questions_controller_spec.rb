847e2010655c8c3a4d9b6273757387828af7e436
Annika Monari <annikamonari@gmail.com>
2014-10-22 14:45:34 +0100

    fixed last of failing tests

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -33,7 +33,7 @@ describe QuestionsController do
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
   # QuestionsController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
+  let(:valid_session) { {id: FactoryGirl.create(:idea).id} }
 
   before(:each) {
     @user = User.find(valid_attributes[:user])
@@ -50,7 +50,7 @@ describe QuestionsController do
 
   describe "GET new" do
     it "assigns a new question as @question" do
-      get :new, {:idea_id => valid_attributes[:idea].id}, valid_session
+      get :new, {:id => valid_attributes[:idea].id}, valid_session
       assigns(:question).should be_a_new(Question)
     end
   end
@@ -67,18 +67,18 @@ describe QuestionsController do
     describe "with valid params" do
       it "creates a new Question" do
         expect {
-          post :create, :question => valid_attributes, :idea_id => valid_attributes[:idea].id
+          post :create, :question => valid_attributes, :id => valid_attributes[:idea].id
         }.to change(Question, :count).by(1)
       end
 
       it "assigns a newly created question as @question" do
-        post :create, :question => valid_attributes, :idea_id => valid_attributes[:idea].id
+        post :create, :question => valid_attributes, :id => valid_attributes[:idea].id
         assigns(:question).should be_a(Question)
         assigns(:question).should be_persisted
       end
 
       it "redirects to the created question" do
-        post :create, :question => valid_attributes, :idea_id => valid_attributes[:idea].id
+        post :create, :question => valid_attributes, :id => valid_attributes[:idea].id
         response.should redirect_to(Question.last)
       end
     end
@@ -87,14 +87,14 @@ describe QuestionsController do
       it "assigns a newly created but unsaved question as @question" do
         # Trigger the behavior that occurs when invalid params are submitted
         Question.any_instance.stub(:save).and_return(false)
-        post :create, {:question => { "title" => "invalid value" }}, valid_session
+        post :create, {:question => { "title" => "invalid value" }, :id => valid_attributes[:idea].id}, valid_session
         assigns(:question).should be_a_new(Question)
       end
 
       it "re-renders the 'new' template" do
         # Trigger the behavior that occurs when invalid params are submitted
         Question.any_instance.stub(:save).and_return(false)
-        post :create, {:question => { "title" => "invalid value" }}, valid_session
+        post :create, {:question => { "title" => "invalid value" }, :id => valid_attributes[:idea].id}, valid_session
         response.should render_template("new")
       end
     end
@@ -141,21 +141,6 @@ describe QuestionsController do
         put :update, {:id => question.to_param, :question => { "title" => "invalid value" }}, valid_session
         response.should render_template("edit")
       end
-    end
-  end
-
-  describe "DELETE destroy" do
-    it "destroys the requested question" do
-      question = Question.create! valid_attributes
-      expect {
-        delete :destroy, {:id => question.to_param}, valid_session
-      }.to change(Question, :count).by(-1)
-    end
-
-    it "redirects to the questions list" do
-      question = Question.create! valid_attributes
-      delete :destroy, {:id => question.to_param}, valid_session
-      response.should redirect_to(questions_url)
     end
   end
 
