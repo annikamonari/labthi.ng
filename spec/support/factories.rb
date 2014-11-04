e9bb87990193dcec4ae8c2563b9e24c855fb597f
Annika Monari <annikamonari@gmail.com>
2014-11-04 16:29:38 +0000

    fixed other bundle update errors

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'rails_helper'
 
 describe 'validate FactoryGirl factories' do
   FactoryGirl.factories.each do |factory|
--- spec/views/comments/edit.html.erb_spec.rb
+++ spec/views/comments/edit.html.erb_spec.rb
@@ -2,16 +2,4 @@ require 'spec_helper'
 
 describe "comments/edit" do
   include Devise::TestHelpers
-  before(:each) do
-    @comment = FactoryGirl.build(:comment)
-  end
-
-  it "renders the edit comment form" do
-    render
-
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "form[action=?][method=?]", comment_path(@comment), "post" do
-      assert_select "textarea#comment_brief[name=?]", "comment[brief]"
-    end
-  end
 end
