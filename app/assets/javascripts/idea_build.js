f5d83b39162b30808855f2f273ed9a55ea0625a0
Annika Monari <annikamonari@gmail.com>
2014-11-05 13:31:01 +0000

    javascript on build page

---
+++ app/assets/javascripts/idea_build.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $("#planlink").click(function() {
+        $('#plan').toggle()
+  });
+});
+
+$(document).ready(function() {
+    $("#bplink").click(function() {
+        $('#bp').toggle()
+  });
+});
+
+$(document).ready(function() {
+    $("#prototypelink").click(function() {
+        $('#prototype').toggle()
+  });
+});
+
+$(document).ready(function() {
+    $("#designlink").click(function() {
+        $('#design').toggle()
+  });
+});
