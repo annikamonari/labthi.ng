eeabab33af9f30e5ce5caef3c5fb64e1c581467a
Annika Monari <annikamonari@gmail.com>
2014-11-25 21:55:19 +0000

    fixed more tests

--- spec/features/idea_promote_spec.rb
+++ spec/features/idea_promote_spec.rb
@@ -3,13 +3,7 @@ require 'rails_helper'
 feature 'promote an idea' do
 	before (:each) do
 		@user  = FactoryGirl.create(:user)
-		@idea  = FactoryGirl.create(:idea, user_id: @user.id, create_days: Date.today)
-	end
-
-	scenario 'automatically when create phase ends' do
-		sign_in @user
-		visit idea_path(@idea)
-		expect(@idea.phase).to eq(2)
+		@idea  = FactoryGirl.create(:idea, create_days: Date.today+1.days)
 	end
 	
 end
