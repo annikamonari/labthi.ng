aa3c92a1b2bfbea7bed6d521a84df8b215fe673d
Annika Monari <annikamonari@gmail.com>
2014-12-01 16:44:19 +0000

    progress bars for idea bulids

--- app/models/component.rb
+++ app/models/component.rb
@@ -4,4 +4,13 @@ class Component < ActiveRecord::Base
 
   # child classes implement create_required_parts
   after_create :create_required_parts
+
+  def get_progress
+    total_equity = 0
+    done_equity  = 0
+
+    self.parts.each { |part| total_equity += part.equity.to_i }
+    self.parts.where(status: 'Accepted').each { |part| done_equity += part.equity.to_i }
+    ('%.0f' % ((done_equity / total_equity.to_f) * 100)).to_i
+  end
 end
